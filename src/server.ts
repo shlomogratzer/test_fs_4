df2bcaf332685a59a68650599d9708722e7f2edf
shlomo <shlomogratzer1@gmail.com>
2024-09-26 09:55:37 +0300

    first commit

---
+++ src/server.ts
@@ -0,0 +1,18 @@
+// Import the 'express' module along with 'Request' and 'Response' types from express
+import express from 'express';
+
+// Create an Express application
+const app = express();
+
+
+
+
+
+// Specify the port number for the server
+const port: number | string= 3000;
+
+// Start the server and listen on the specified port
+app.listen(port, () => {
+  // Log a message when the server is successfully running
+  console.log(`Server is running on http://localhost:${port}`);
+});
