86c09157193e5a8961052f515b02be770b4f4bce
shlomo <shlomogratzer1@gmail.com>
2024-09-26 12:09:38 +0300

    fuctionality create and get

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,19 @@
 // Import the 'express' module along with 'Request' and 'Response' types from express
-import express from 'express';
-
+import express, {Application}from 'express';
+import dotenv from 'dotenv'
+import router from './routs/beepersRouter';
+dotenv.config()
 // Create an Express application
-const app = express();
+const app:Application = express();
 
 
 
 
 
 // Specify the port number for the server
-const port: number | string= 3000;
-
+const port : number | string = process.env.PORT || 3009;
+app.use(express.json())
+app.use(router)
 // Start the server and listen on the specified port
 app.listen(port, () => {
   // Log a message when the server is successfully running
